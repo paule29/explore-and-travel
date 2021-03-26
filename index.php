1c69074da467917aa143fbdec1514f1700235dd5
Paule29 <pauleombiono@gmail.com>
2021-03-26 15:57:04 +0100

    début de l'intégration

--- index.php
+++ index.php
@@ -1,3 +1,55 @@
 <?php include "header.php" ?>
 
+<div class="container">
+
+<!-- Début de la navbar -->
+
+<nav class="navbar navbar-expand-lg navbar-light pb-5">
+<div class="container-fluid">
+<a class="navbar-brand" href="#">
+<img src="images/logo.png" alt="">
+</a>
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarText">
+<span class="navbar-nav me-auto mb-2 mb-lg-0">
+</span>
+<span class="">
+<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+<li class="nav-item">
+<a class="nav-link active" aria-current="page" href="#">Home</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Features</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Pricing</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Pricing</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Pricing</a>
+</li>
+</ul>
+</span>
+</div>
+</div>
+</nav>
+
+<!-- Fin de la navbar -->
+
+    <div class="row">
+        <div class="col-md-6 col-sm-12">
+            <h1>Explore and travel</h1>
+
+        </div>
+        <div class="col-md-6 col-sm-12">
+            <img src="images/thousand-01.png" alt="">
+        </div>
+    </div>
+</div>
+
+
 <?php include "footer.php" ?>
