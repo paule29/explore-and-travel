087f59be12aca6e7a81e703413f437a5b9fe7895
Paule29 <pauleombiono@gmail.com>
2021-03-26 15:28:59 +0100

    structure html + test de la grille boothstrap

---
+++ grid-exemple.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Integration d'une page web sur la base d'une maquette réalisée par un graphiste.">
+    <title>Document</title>
+    <link rel="shortcut icon" href="images/logo.png" type="image/">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <div class="container-fluid">
+
+        <div class="row">
+
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            
+        </div>
+
+        <div class="row">
+
+            <div class="col-md-4 col-sm-12 dotted-border">col-4</div>
+            <div class="col-md-8 col-sm-12 dotted-border">col-8</div>
+        </div>
+
+    </div>
+
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+            
+        </div>
+
+
+        <div class="row">
+            <div class="col-md-4 col-sm-12 dotted-border">col-4</div>
+            <div class="col-md-8 col-sm-12 dotted-border">col-8</div>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-6 col-sm-12 dotted-border">col-6</div>
+            <div class="col-md-6 col-sm-12 dotted-border">
+
+                <div class="row">
+
+                    <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+                    <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+                    <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+                    <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+                    <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+                    <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+                    <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+                    <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+                    <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+                    <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+                    <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+                    <div class="col-md-1 col-sm-12 dotted-border">col-1</div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+</body>
+</html>
